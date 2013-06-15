4295d4971fdb07482865e032f87e48fa6374ef03
Ryan Peters <ryanpete@lab126.com>
2013-06-15 13:23:24 -0700

    Added swarm api

--- MiningTurtle/slave.lua
+++
@@ -1,5 +0,0 @@
--- For a turtle to run, accepts commands from the taskMaster
-require"ryan_api"
-local modem = peripheral.wrap("right")
-modem.transmit(1, 2, "New slave")
-local event, modemSide, senderChanel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
